7091a25ff3de091f6ba814275c4358bf6be4e194
Dmitry Popov <dmitry@parsec-club.ru>
2015-06-25 19:33:47 +0300

    Demo app improved a little

--- TelegramBotDemo/Program.cs
+++ TelegramBotDemo/Program.cs
@@ -7,6 +7,8 @@ namespace TelegramBotDemo
 {
     public class Program
     {
+        private static bool stopMe = false;
+
         public static void Main(string[] args)
         {
             var accessToken = ConfigurationManager.AppSettings["AccessToken"];
@@ -14,32 +16,50 @@ namespace TelegramBotDemo
             Console.WriteLine("Starting your bot...");
             Console.WriteLine();
 
+            var t = Task.Run(() => RunBot(accessToken));
+
+            Console.ReadLine();
+            stopMe = true;
+        }
+
+        public static void RunBot(string accessToken)
+        {
             var bot = new TelegramBot(accessToken);
 
             var me = bot.GetMe();
-            if (me != null)
+            if (me == null)
             {
-                Console.WriteLine("Me: {0} (@{1})", me.FirstName, me.Username);
+                Console.WriteLine("GetMe() FAILED. Do you forget to add your AccessToken to App.config?");
+                Console.WriteLine("(Press ENTER to quit)");
             }
-            var updates = bot.GetUpdates(0);
-            if (updates != null)
+            Console.WriteLine("{0} (@{1}) connected!", me.FirstName, me.Username);
+
+            Console.WriteLine();
+            Console.WriteLine("Find @{0} in Telegram and send him a message - it will be displayed here", me.Username);
+            Console.WriteLine("(Press ENTER to stop listening and quit)");
+            Console.WriteLine();
+
+            long offset = 0;
+            while (!stopMe)
             {
-                foreach (var update in updates)
+                var updates = bot.GetUpdates(offset);
+                if (updates != null)
                 {
-                    if (update.Message != null) {
-                        Console.WriteLine("Msg from {0} {1} ({2}): {3}",
-                            update.Message.From.FirstName,
-                            update.Message.From.LastName,
-                            update.Message.From.Username,
-                            update.Message.Text
-                            );
+                    foreach (var update in updates)
+                    {
+                        offset = update.UpdateId + 1;
+                        if (update.Message != null)
+                        {
+                            Console.WriteLine("Msg from {0} {1} ({2}): {3}",
+                                update.Message.From.FirstName,
+                                update.Message.From.LastName,
+                                update.Message.From.Username,
+                                update.Message.Text
+                                );
+                        }
                     }
                 }
             }
-
-            Console.WriteLine();
-            Console.WriteLine("Done. Press ENTER to quit");
-            Console.ReadLine();
         }
     }
 }
