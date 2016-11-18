78acde48a2ab7c9dca353a53ae8808bb325ff7db
Dmitry Popov <dmitry@parsec-club.ru>
2016-11-18 17:53:56 +0300

    MessageEntityType enum and MessageEntity.GetMessageEntityType() added (from API 2.1, May 22 2016)

--- NetTelegramBotApi/Types/MessageEntity.cs
+++ NetTelegramBotApi/Types/MessageEntity.cs
@@ -3,15 +3,53 @@
 namespace NetTelegramBotApi.Types
 {
     /// <summary>
-    /// This object represents one special entity in a text message. For example, hashtags, usernames, URLs, etc. 
+    /// This object represents one special entity in a text message. For example, hashtags, usernames, URLs, etc.
     /// </summary>
     public class MessageEntity
     {
         /// <summary>
-        /// Type of the entity. 
+        /// Type of the entity.
         /// Can be mention (@username), hashtag, bot_command, url, email, bold (bold text), italic (italic text), code (monowidth string), pre (monowidth block), text_link (for clickable text URLs), text_mention (for users without usernames)
         /// </summary>
         public string Type { get; set; }
+
+        /// <summary>
+        /// Returns one of <see cref="MessageEntityType"/> members, based on chat <see cref="Type"/> value
+        /// </summary>
+        /// <returns></returns>
+        public MessageEntityType GetMessageEntityType()
+        {
+            // special case: mention(@username)
+            if (Type.StartsWith("mention", StringComparison.OrdinalIgnoreCase))
+            {
+                return MessageEntityType.Mention;
+            }
+            switch (Type.ToLowerInvariant())
+            {
+                case "hashtag":
+                    return MessageEntityType.Hashtag;
+                case "bot_command":
+                    return MessageEntityType.BotCommand;
+                case "url":
+                    return MessageEntityType.Url;
+                case "email":
+                    return MessageEntityType.Email;
+                case "bold":
+                    return MessageEntityType.Bold;
+                case "italic":
+                    return MessageEntityType.Italic;
+                case "code":
+                    return MessageEntityType.Code;
+                case "pre":
+                    return MessageEntityType.Pre;
+                case "text_link":
+                    return MessageEntityType.TextLink;
+                case "text_mention":
+                    return MessageEntityType.TextMention;
+                default:
+                    return MessageEntityType.Unknown;
+            }
+        }
 
         /// <summary>
         /// Offset in UTF-16 code units to the start of the entity
---
+++ NetTelegramBotApi/Types/MessageEntityType.cs
@@ -0,0 +1,18 @@
+﻿namespace NetTelegramBotApi.Types
+{
+    public enum MessageEntityType : byte
+    {
+        Unknown = 0,
+        Mention = 1,
+        Hashtag = 2,
+        BotCommand = 3,
+        Url = 4,
+        Email = 5,
+        Bold = 6,
+        Italic = 7,
+        Code = 8,
+        Pre = 9,
+        TextLink = 10,
+        TextMention = 11
+    }
+}
