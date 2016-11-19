867f1a50fda855c66ec693f392d0df179e41e0de
Dmitry Popov <dmitry@parsec-club.ru>
2016-11-20 01:56:33 +0300

    Added new field switch_inline_query_current_chat in InlineKeyboardButton. Also, CallbackGame added

---
+++ NetTelegramBotApi/Types/CallbackGame.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NetTelegramBotApi.Types
+{
+    /// <summary>
+    /// A placeholder, currently holds no information. Use BotFather to set up your game.
+    /// </summary>
+    public class CallbackGame
+    {
+        // Nothing
+    }
+}
--- NetTelegramBotApi/Types/InlineKeyboardButton.cs
+++ NetTelegramBotApi/Types/InlineKeyboardButton.cs
@@ -26,15 +26,29 @@ namespace NetTelegramBotApi.Types
         public string CallbackData { get; set; }
 
         /// <summary>
-        /// Optional. If set, pressing the button will prompt the user to select one of their chats, 
-        /// open that chat and insert the bot‘s username and the specified inline query in the input field. 
+        /// Optional. If set, pressing the button will prompt the user to select one of their chats,
+        /// open that chat and insert the bot‘s username and the specified inline query in the input field.
         /// Can be empty, in which case just the bot’s username will be inserted.
         /// </summary>
         /// <remarks>
-        /// Note: This offers an easy way for users to start using your bot in inline mode when they are currently 
-        /// in a private chat with it. Especially useful when combined with switch_pm… actions – in this case the user 
+        /// Note: This offers an easy way for users to start using your bot in inline mode when they are currently
+        /// in a private chat with it. Especially useful when combined with switch_pm… actions – in this case the user
         /// will be automatically returned to the chat they switched from, skipping the chat selection screen.
         /// </remarks>
         public string SwitchInlineQuery { get; set; }
+
+        /// <summary>
+        /// Optional. If set, pressing the button will insert the bot‘s username and the specified inline query in the current chat's input field.
+        /// Can be empty, in which case only the bot’s username will be inserted.
+        /// This offers a quick way for the user to open your bot in inline mode
+        /// in the same chat – good for selecting something from multiple options.
+        /// </summary>
+        public string SwitchInlineQueryCurrentChat { get; set; }
+
+        /// <summary>
+        /// Optional. Description of the game that will be launched when the user presses the button.
+        /// NOTE: This type of button must always be the first button in the first row.
+        /// </summary>
+        public CallbackGame CallbackGame { get; set; }
     }
 }
