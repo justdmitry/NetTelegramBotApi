1fa9d802d986fab8542631ebc459f4c880cfa077
Dmitry Popov <dmitry@parsec-club.ru>
2017-05-23 15:47:28 +0300

    Fix issues #52 and #53

--- NetTelegramBotApi/Requests/SetWebhook.cs
+++ NetTelegramBotApi/Requests/SetWebhook.cs
@@ -6,14 +6,14 @@ using NetTelegramBotApi.Types;
 namespace NetTelegramBotApi.Requests
 {
     /// <summary>
-    /// Use this method to specify a url and receive incoming updates via an outgoing webhook. 
-    /// Whenever there is an update for the bot, we will send an HTTPS POST request to the specified url, 
-    /// containing a JSON-serialized Update. In case of an unsuccessful request, we will give up after 
+    /// Use this method to specify a url and receive incoming updates via an outgoing webhook.
+    /// Whenever there is an update for the bot, we will send an HTTPS POST request to the specified url,
+    /// containing a JSON-serialized Update. In case of an unsuccessful request, we will give up after
     /// a reasonable amount of attempts.
     /// </summary>
     /// <remarks>
-    /// If you'd like to make sure that the Webhook request comes from Telegram, 
-    /// we recommend using a secret path in the URL, e.g. www.example.com/<secret_path>. 
+    /// If you'd like to make sure that the Webhook request comes from Telegram,
+    /// we recommend using a secret path in the URL, e.g. www.example.com/<secret_path>.
     /// Since nobody else knows this secret_path, you can be pretty sure it’s us.
     /// </remarks>
     public class SetWebhook : SendFileRequestBase<bool>
@@ -43,9 +43,21 @@ namespace NetTelegramBotApi.Requests
         /// </summary>
         public string Url { get; set; }
 
+        /// <summary>
+        /// Maximum allowed number of simultaneous HTTPS connections to the webhook for update delivery, 1-100.
+        /// Defaults to 40.
+        /// Use lower values to limit the load on your bot‘s server, and higher values to increase your bot’s throughput.
+        /// </summary>
+        public byte? MaxConnections { get; set; }
+
         protected override void AppendParameters(Action<string, string> appendCallback)
         {
             appendCallback("url", Url);
+
+            if (MaxConnections.HasValue)
+            {
+                appendCallback("max_connections", MaxConnections.Value.ToString());
+            }
 
             base.AppendParameters(appendCallback);
         }
