4436570dc97c801c2efeb1f9e0f2acfff4135876
Dmitry Popov <dmitry@parsec-club.ru>
2023-02-14 12:02:42 +0300

    v5.2: User and Chat updated to actual Bot API

--- NetTelegramBotApi/Types/Chat.cs
+++ NetTelegramBotApi/Types/Chat.cs
@@ -13,54 +13,160 @@ namespace NetTelegramBotApi.Types
         public long Id { get; set; }
 
         /// <summary>
-        /// Type of chat, can be either “private”, “group”, “supergroup” or “channel”
+        /// Type of chat, can be either “private”, “group”, “supergroup” or “channel”. See also <seealso cref="GetChatType"/>
         /// </summary>
         public string Type { get; set; }
 
         /// <summary>
-        /// Returns one of 'ChatType' members, based on chat 'Type' value
+        /// Returns one of 'ChatType' members, based on chat 'Type' value,
         /// </summary>
         /// <returns></returns>
         public ChatType GetChatType()
         {
-            switch (Type)
+            return Type switch
             {
-                case "private":
-                    return ChatType.Private;
-                case "group":
-                    return ChatType.Group;
-                case "supergroup":
-                    return ChatType.Supergroup;
-                case "channel":
-                    return ChatType.Channel;
-                default:
-                    return ChatType.Unknown;
-            }
+                "private" => ChatType.Private,
+                "group" => ChatType.Group,
+                "supergroup" => ChatType.Supergroup,
+                "channel" => ChatType.Channel,
+                _ => ChatType.Unknown,
+            };
         }
 
         /// <summary>
-        /// Optional. Title, for channels and group chats
+        /// Optional. Title, for supergroups, channels and group chats.
         /// </summary>
         public string Title { get; set; }
 
         /// <summary>
-        /// Optional. Username, for private chats, supergroups and channels if available
-        /// </summary>
-        public string FirstName { get; set; }
-
-        /// <summary>
-        /// Optional. First name of the other party in a private chat
-        /// </summary>
-        public string LastName { get; set; }
-
-        /// <summary>
-        /// Optional. Last name of the other party in a private chat
+        /// Optional. Username, for private chats, supergroups and channels if available.
         /// </summary>
         public string Username { get; set; }
 
         /// <summary>
+        /// Optional. First name of the other party in a private chat.
+        /// </summary>
+        public string FirstName { get; set; }
+
+        /// <summary>
+        /// Optional. Last name of the other party in a private chat.
+        /// </summary>
+        public string LastName { get; set; }
+
+        /// <summary>
+        /// Optional. True, if the supergroup chat is a forum (has topics enabled)
+        /// </summary>
+        public bool IsForum { get; set; }
+
+        /// <summary>
+        /// NOT IMPLEMENTED. Optional. Chat photo. Returned only in getChat.
+        /// </summary>
+        public object Photo { get; set; }
+
+        /// <summary>
+        /// Optional. If non-empty, the list of all active chat usernames; for private chats, supergroups and channels. Returned only in getChat.
+        /// </summary>
+        public string[] ActiveUsernames { get; set; }
+
+        /// <summary>
+        /// Optional. Custom emoji identifier of emoji status of the other party in a private chat. Returned only in getChat.
+        /// </summary>
+        public string EmojiStatusCustomEmojiId { get; set; }
+
+        /// <summary>
+        /// Optional. Bio of the other party in a private chat. Returned only in getChat.
+        /// </summary>
+        public string Bio { get; set; }
+
+        /// <summary>
+        /// Optional. True, if privacy settings of the other party in the private chat allows to use tg://user?id=<user_id> links only in chats with the user. Returned only in getChat.
+        /// </summary>
+        public bool HasPrivateForwards { get; set; }
+
+        /// <summary>
+        /// Optional. True, if the privacy settings of the other party restrict sending voice and video note messages in the private chat. Returned only in getChat.
+        /// </summary>
+        public bool HasRestrictedVoiceAndVideoMessages { get; set; }
+
+        /// <summary>
+        /// Optional. True, if users need to join the supergroup before they can send messages. Returned only in getChat.
+        /// </summary>
+        public bool JoinToSendMessages { get; set; }
+
+        /// <summary>
+        /// Optional. True, if all users directly joining the supergroup need to be approved by supergroup administrators. Returned only in getChat.
+        /// </summary>
+        public bool JoinByRequest { get; set; }
+
+        /// <summary>
+        /// Optional. Description, for groups, supergroups and channel chats. Returned only in getChat.
+        /// </summary>
+        public string Description { get; set; }
+
+        /// <summary>
+        /// Optional. Primary invite link, for groups, supergroups and channel chats. Returned only in getChat.
+        /// </summary>
+        public string InviteLink { get; set; }
+
+        /// <summary>
+        /// Optional. The most recent pinned message (by sending date). Returned only in getChat.
+        /// </summary>
+        public Message PinnedMessage { get; set; }
+
+        /// <summary>
+        /// NOT IMPLEMENTED. Optional. Default chat member permissions, for groups and supergroups. Returned only in getChat.
+        /// </summary>
+        public object Permissions { get; set; }
+
+        /// <summary>
+        /// Optional. For supergroups, the minimum allowed delay between consecutive messages sent by each unpriviledged user; in seconds. Returned only in getChat.
+        /// </summary>
+        public int SlowModeDelay { get; set; }
+
+        /// <summary>
+        /// Optional. The time after which all messages sent to the chat will be automatically deleted; in seconds. Returned only in getChat.
+        /// </summary>
+        public int MessageAutoDeleteTime { get; set; }
+
+        /// <summary>
+        /// Optional. True, if aggressive anti-spam checks are enabled in the supergroup. The field is only available to chat administrators. Returned only in getChat.
+        /// </summary>
+        public bool HasAggressiveAntiSpamEnabled { get; set; }
+
+        /// <summary>
+        /// Optional. True, if non-administrators can only get the list of bots and administrators in the chat. Returned only in getChat.
+        /// </summary>
+        public bool HasHiddenMembers { get; set; }
+
+        /// <summary>
+        /// Optional. True, if messages from the chat can't be forwarded to other chats. Returned only in getChat.
+        /// </summary>
+        public bool HasProtectedContent { get; set; }
+
+        /// <summary>
+        /// Optional. For supergroups, name of group sticker set. Returned only in getChat.
+        /// </summary>
+        public string StickerSetName { get; set; }
+
+        /// <summary>
+        /// Optional. True, if the bot can change the group sticker set. Returned only in getChat.
+        /// </summary>
+        public bool CanSetStickerSet { get; set; }
+
+        /// <summary>
+        /// Optional. Unique identifier for the linked chat, i.e. the discussion group identifier for a channel and vice versa; for supergroups and channel chats. Returned only in getChat.
+        /// </summary>
+        public long LinkedChatId { get; set; }
+
+        /// <summary>
+        /// NOT IMPLEMENTED. Optional. For supergroups, the location to which the supergroup is connected. Returned only in getChat.
+        /// </summary>
+        public object Location { get; set; }
+
+        /// <summary>
         /// Optional. True if a group has ‘All Members Are Admins’ enabled.
         /// </summary>
+        [Obsolete("Removed from Telegram API")]
         public bool AllMembersAreAdministrators { get; set; }
     }
 }
--- NetTelegramBotApi/Types/User.cs
+++ NetTelegramBotApi/Types/User.cs
@@ -13,18 +13,53 @@ namespace NetTelegramBotApi.Types
         public long Id { get; set; }
 
         /// <summary>
-        /// User‘s or bot’s first name
+        /// True, if this user is a bot.
+        /// </summary>
+        public bool IsBot { get; set; }
+
+        /// <summary>
+        /// User's or bot's first name
         /// </summary>
         public string FirstName { get; set; }
 
         /// <summary>
-        /// Optional. User‘s or bot’s last name
+        /// Optional. User's or bot's last name
         /// </summary>
         public string LastName { get; set; }
 
         /// <summary>
-        /// Optional. User‘s or bot’s username
+        /// Optional. User's or bot's username
         /// </summary>
         public string Username { get; set; }
+
+        /// <summary>
+        /// Optional. IETF language tag of the user's language
+        /// </summary>
+        public string LanguageCode { get; set; }
+
+        /// <summary>
+        /// Optional. True, if this user is a Telegram Premium user.
+        /// </summary>
+        public bool IsPremium { get; set; }
+
+        /// <summary>
+        /// Optional. True, if this user added the bot to the attachment menu.
+        /// </summary>
+        public bool AddedToAttachmentMenu { get; set; }
+
+        /// <summary>
+        /// Optional. True, if the bot can be invited to groups. Returned only in getMe.
+        /// </summary>
+        public bool CanJoinGroups { get; set; }
+
+        /// <summary>
+        /// Optional. True, if privacy mode is disabled for the bot. Returned only in getMe.
+        /// </summary>
+        public bool CanReadAllGroupMessages { get; set; }
+
+        /// <summary>
+        /// Optional. True, if the bot supports inline queries. Returned only in getMe.
+        /// </summary>
+        public bool SupportsInlineQueries { get; set; }
     }
 }
