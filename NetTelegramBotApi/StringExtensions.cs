67ee03f797f2348cd801fcb97464b4c1066f06f8
Dmitry Popov <dmitry@parsec-club.ru>
2024-10-23 21:12:55 +0300

    feat: string.MarkdownV2Encode

--- NetTelegramBotApi/StringExtensions.cs
+++ NetTelegramBotApi/StringExtensions.cs
@@ -1,19 +1,53 @@
 ﻿namespace System
 {
-    using System.Text.RegularExpressions;
+    using System.Diagnostics.CodeAnalysis;
+    using System.Linq;
+    using System.Text;
 
     public static class StringExtensions
     {
-        private static readonly Regex WrongChars = new(@"([\*_`\[\]\(\)])");
+        private static readonly char[] MarkdownV1Chars = ['\\', '_', '*', '`', '['];
 
+        private static readonly char[] MarkdownV2Chars = ['\\', '_', '*', '[', ']', '(', ')', '~', '`', '>', '#', '+', '-', '=', '|', '{', '}', '.', '!'];
+
+        [return: NotNullIfNotNull(nameof(value))]
         public static string MarkdownEncode(this string value)
         {
-            if (string.IsNullOrWhiteSpace(value))
+            return Escape(value, MarkdownV1Chars);
+        }
+
+        [return: NotNullIfNotNull(nameof(value))]
+        public static string MarkdownV2Encode(this string value)
+        {
+            return Escape(value, MarkdownV2Chars);
+        }
+
+        [return: NotNullIfNotNull(nameof(source))]
+        private static string Escape(string source, char[] characters)
+        {
+            if (string.IsNullOrWhiteSpace(source))
             {
-                return value;
+                return source;
             }
 
-            return WrongChars.Replace(value, @"\$1");
+            if (source.IndexOfAny(characters) == -1)
+            {
+                return source;
+            }
+
+            var sb = new StringBuilder(source.Length + 50);
+            foreach (var c in source)
+            {
+                if (characters.Contains(c))
+                {
+                    sb.Append('\\');
+                }
+
+                sb.Append(c);
+            }
+
+            return sb.ToString();
         }
+
     }
 }
